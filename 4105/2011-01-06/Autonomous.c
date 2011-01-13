0a5de67fe586938bb93a656c2ab038e6e80231c5
4105 <4105ftc@gmail.com>
2011-01-13 00:21:30 +0000

    initial import
    git-svn-id: https://dev.shelftagger.com/svn/ftc@1 0ddc33c5-d9b6-4607-9548-e4c7f036457c

---
+++ 4105/2011-01-06/Autonomous.c
@@ -0,0 +1,146 @@
+/* Autonomous.c */
+
+#include "Hardware.h"
+#include "Autonomous.h"
+
+/**
+ * Initialize robot.
+ */
+void initializeRobot()
+{
+  // Place code here to sinitialize servos to starting positions.
+  // Sensors are automatically configured and setup by ROBOTC. They may need a brief time to stabilize.
+  //servo[wheelieBar1] = 0;
+  //servo[wheelieBar2] = 0;
+  //bFloatDuringInactiveMotorPWM = false;
+  motor[motorA] = 100;
+  motor[motorB] = 100;
+  motor[motorC] = 100;
+
+  return;
+}
+
+/**
+ * Make a right turn.
+ */
+void rightTurn()
+{
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+  while (abs(nMotorEncoder[motorE]) < RIGHT_TURN_ENCODER ||
+         abs(nMotorEncoder[motorD]) < RIGHT_TURN_ENCODER)
+  {
+    motor[motorE] = TURN_SPEED;
+    motor[motorD] = TURN_SPEED * -1;
+  }
+  motor[motorE] = 0;
+  motor[motorD] = 0;
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+}
+
+/**
+ * Make a left turn.
+ */
+void leftTurn()
+{
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+  while (abs(nMotorEncoder[motorD]) < LEFT_TURN_ENCODER ||
+         abs(nMotorEncoder[motorE]) < LEFT_TURN_ENCODER)
+  {
+    motor[motorE] = TURN_SPEED * -1;
+    motor[motorD] = TURN_SPEED;
+  }
+  motor[motorE] = 0;
+  motor[motorD] = 0;
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+}
+
+/**
+ * Move the provided distance at the provided motor speed.
+ *
+ * distance -- the distance in encoder counts
+ * speed    -- the motor speed (positive == forwards, negative == reverse
+ */
+void move(int distance, int speed)
+{
+  while (abs(nMotorEncoder[motorD]) < distance ||
+         abs(nMotorEncoder[motorE]) < distance)
+  {
+    motor[motorE] = speed;
+    motor[motorD] = speed;
+  }
+  motor[motorE] = 0;
+  motor[motorD] = 0;
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+}
+
+/**
+ * Put the wheelie bar down.
+ */
+void wheelieBarDown()
+{
+  motor[wheelieBar] = WHEELIE_BAR_SPEED * -1;
+  wait1Msec(WHEELIE_BAR_TIME);
+}
+
+/**
+ * Put the wheelie bar up.
+ */
+void wheelieBarUp()
+{
+  motor[wheelieBar] = WHEELIE_BAR_SPEED;
+  wait1Msec(WHEELIE_BAR_TIME);
+}
+
+/**
+ * Make a left quarter turn.
+ */
+void leftQuarterTurn()
+{
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+  while (abs(nMotorEncoder[motorD]) < LEFT_TURN_ENCODER / 2 ||
+         abs(nMotorEncoder[motorE]) < LEFT_TURN_ENCODER / 2)
+  {
+    motor[motorE] = TURN_SPEED * -1;
+    motor[motorD] = TURN_SPEED;
+  }
+  motor[motorE] = 0;
+  motor[motorD] = 0;
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+}
+
+/**
+ * Make a right quarter turn.
+ */
+void rightQuarterTurn()
+{
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+  while (abs(nMotorEncoder[motorD]) < RIGHT_TURN_ENCODER / 2 ||
+         abs(nMotorEncoder[motorE]) < RIGHT_TURN_ENCODER / 2)
+  {
+    motor[motorE] = TURN_SPEED;
+    motor[motorD] = TURN_SPEED * -1;
+  }
+  motor[motorE] = 0;
+  motor[motorD] = 0;
+  nMotorEncoder[motorE] = 0;
+  nMotorEncoder[motorD] = 0;
+}
+
+/**
+ * Run the baton loader for 5 seconds.
+ */
+void batonLoader()
+{
+  // baton (re)loader
+  motor[intake] = INTAKE_SPEED;
+  wait1Msec(5000);
+  motor[intake] = 0;
+}
---
+++ 4105/2011-01-09/R-Bridge.c
@@ -0,0 +1,53 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Motor,  motorA,          motorA,        tmotorNormal, openLoop)
+#pragma config(Motor,  motorB,          motorB,        tmotorNormal, openLoop)
+#pragma config(Motor,  motorC,          motorC,        tmotorNormal, openLoop)
+#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorNormal, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorNormal, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     intake,        tmotorNormal, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     wheelieBar,    tmotorNormal, openLoop)
+#pragma config(Servo,  srvo_S1_C3_3,    goalArm,              tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_1,    goalCapture1,          tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_2,    goalCapture2,          tServoStandard)
+
+/* R-Bridge */
+
+#include "JoystickDriver.c"  //Include file to "handle" the Bluetooth messages.
+#include "Autonomous.c"
+
+/**
+ *
+ */
+task main()
+{
+  initializeRobot();
+
+  waitForStart(); // Wait for the beginning of autonomous phase.
+
+  nMotorEncoder[motorE] = 0;  //clear the TETRIX encoders in motors D and E
+  nMotorEncoder[motorD] = 0;
+
+  motor[motorE] = 0; //turn both motors off
+  motor[motorD] = 0;
+
+  wait1Msec(1000);
+
+  leftTurn();
+  wait1Msec(600);
+
+  move(2000, 30);
+  wait1Msec(600);
+
+  rightTurn();
+  wait1Msec(600);
+
+  move(4000, 30);
+  wait1Msec(600);
+
+  /* wheelie bar */
+  wheelieBarDown();
+  wheelieBarUp();
+
+  move(4000, 30);
+  wait1Msec(600);
+}
