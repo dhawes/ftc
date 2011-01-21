00ac72103d10e6f710c4b305d6b427e184e3c311
4105 <4105ftc@gmail.com>
2011-01-21 05:56:06 +0000

    - delete unused programs - add L-Cliff.c.  Cliff dive!

--- 4105/L-Cliff.c
+++ 4105/L-Cliff.c
@@ -1,4 +1,5 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Sensor, S2,     HTAC,                sensorLowSpeed)
 #pragma config(Motor,  motorA,          motorA,        tmotorNormal, openLoop)
 #pragma config(Motor,  motorB,          motorB,        tmotorNormal, openLoop)
 #pragma config(Motor,  motorC,          motorC,        tmotorNormal, openLoop)
@@ -6,14 +7,14 @@
 #pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorNormal, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     intake,        tmotorNormal, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C2_2,     wheelieBar,    tmotorNormal, openLoop)
-#pragma config(Servo,  srvo_S1_C3_3,    goalArm,              tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_1,    goalCapture1,          tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_2,    goalCapture2,          tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_3,    goalArm,              tServoStandard)
 
-/* LR-Cliff-L.c */
+/* LR-Cliff-LCR.c */
 
-#include "JoystickDriver.c"  //Include file to "handle" the Bluetooth messages.
 #include "Autonomous.c"
+#include "JoystickDriver.c"  //Include file to "handle" the Bluetooth messages.
 
 /**
  *
@@ -24,20 +25,7 @@ task main()
 
   waitForStart(); // Wait for the beginning of autonomous phase.
 
-  nMotorEncoder[motorE] = 0;  //clear the TETRIX encoders in motors D and E
-  nMotorEncoder[motorD] = 0;
-
-  motor[motorE] = 0; //turn both motors off
-  motor[motorD] = 0;
-
-  wait1Msec(1000);
-
   // 1 rotation is about 2', 1440 on the encoder
   move(7000, 50);
   wait1Msec(600);
-
-  leftTurn();
-  wait1Msec(600);
-
-  batonLoader();
 }
